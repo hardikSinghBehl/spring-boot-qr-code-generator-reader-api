11d3dd36db15d2ab017b7df3715fe11166cc9cbe
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-30 12:37:43 +0530

    initial dummy project commit

---
+++ src/main/java/com/hardik/iris/SpringBootQrCodeGeneratorReaderApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.iris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGeneratorReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGeneratorReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/iris/SpringBootQrCodeGeneratorReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.iris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGeneratorReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
